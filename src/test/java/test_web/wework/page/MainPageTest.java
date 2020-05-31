a05cf052385deccc9d2dfa98edb4dff006663300
nilmiao <nil.miu@kucoin.com>
2020-05-31 16:48:32 +0800

    更新添加删除部门用例

--- src/test/java/test_web/wework/page/MainPageTest.java
+++ src/test/java/test_web/wework/page/MainPageTest.java
@@ -2,11 +2,9 @@ package test_web.wework.page;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MainPageTest {
     @Test
-    void mainTest(){
+    void mainTest() {
         new MainPage();
     }
 
