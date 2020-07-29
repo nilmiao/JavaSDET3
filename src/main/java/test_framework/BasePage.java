d78ec5312db481d21a504e90660a3c982f5bf724
nilmiao <nil.miu@kucoin.com>
2020-07-29 23:38:35 +0800

    提交测试框架初

---
+++ src/main/java/test_framework/BasePage.java
@@ -0,0 +1,34 @@
+package test_framework;
+
+/**
+ * @author Miao on 2020/7/29
+ */
+
+// 自动化领域建模
+public class BasePage {
+    public void click(){
+
+    }
+
+    public void find(){
+
+    }
+
+    public void send(){
+
+    }
+
+    public void getText(){
+
+    }
+
+    public void run(){
+
+    }
+
+    public void  load(String path){
+
+    }
+
+
+}
---
+++ src/test/java/test_framework/BasePageTest.java
@@ -0,0 +1,26 @@
+package test_framework;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasePageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void load() {
+    }
+}
